ee9aeb6600f2795ee2aa65655b4aaf9b6ea464a3
akkadaska <akkadaska@gmail.com>
2022-09-25 08:09:13 +0900

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-// const isGithubActions = process.env.GITHUB_ACTIONS || false;
-//
-// let assetPrefix = undefined;
-// let basePath = undefined;
-//
-// if (isGithubActions) {
-//   // trim off `<owner>/`
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?// \//, '');
-//
-//   assetPrefix = `/${repo}/`;
-//   basePath = `/${repo}`;
-// }
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = undefined;
+let basePath = undefined;
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // assetPrefix: assetPrefix,
-  // basePath: basePath,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
 
 module.exports = nextConfig;
